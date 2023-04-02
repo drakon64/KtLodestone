243c361c61761b27a8a1042faf528ac60f6207c1
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-04-02 23:28:14 +0100

    Support Gunbreaker

--- src/main/kotlin/KtLodestone.kt
+++ src/main/kotlin/KtLodestone.kt
@@ -47,6 +47,7 @@ object KtLodestone {
                         .first() !!
                         .attr("src")) {
                         "https://img.finalfantasyxiv.com/lds/h/P/V01m8YRBYcIs5vgbRtpDiqltSE.png" -> "Black Mage"
+                        "https://img.finalfantasyxiv.com/lds/h/8/hg8ofSSOKzqng290No55trV4mI.png" -> "Gunbreaker"
                         "https://img.finalfantasyxiv.com/lds/h/E/vmtbIlf6Uv8rVp2YFCWA25X0dc.png" -> "Machinist"
                         "https://img.finalfantasyxiv.com/lds/h/q/s3MlLUKmRAHy0pH57PnFStHmIw.png" -> "Red Mage"
                         else -> throw ClassJobException("Unknown class/job")
