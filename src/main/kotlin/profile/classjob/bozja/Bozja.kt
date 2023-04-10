3b0f77ebb9cd6e26cfeced36cc3ebfc915b5cf0a
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-04-10 20:08:44 +0100

    Add classes for Bozja

---
+++ src/main/kotlin/profile/classjob/bozja/Bozja.kt
@@ -0,0 +1,3 @@
+package cloud.drakon.ktlodestone.profile.classjob.bozja
+
+data class Bozja(val level: Byte, val mettle: Mettle?)
---
+++ src/main/kotlin/profile/classjob/bozja/Mettle.kt
@@ -0,0 +1,3 @@
+package cloud.drakon.ktlodestone.profile.classjob.bozja
+
+data class Mettle(val current: Int, val next: Int)
