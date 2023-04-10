a6aaa6d12220d7e341b2f786fc56f9d1195473fe
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-04-10 18:37:24 +0100

    Reduce complexity of the `getCharacter` function https://sonarcloud.io/project/issues?open=AYdsBvd0ygVCcmIR4Tyr&id=KtLodestone

--- src/main/kotlin/profile/pvpteam/PvpTeam.kt
+++ src/main/kotlin/profile/pvpteam/PvpTeam.kt
@@ -1,3 +1,3 @@
 package cloud.drakon.ktlodestone.profile.pvpteam
 
-data class PvpTeam(val name: String, val iconLayers: PvpTeamIconLayers)
+data class PvpTeam(val name: String, val id: String, val iconLayers: PvpTeamIconLayers)
