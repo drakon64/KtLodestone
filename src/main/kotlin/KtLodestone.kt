d303fe1b56d423b6bb47e30233ccf554307bf3ee
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-04-30 20:45:21 +0100

    Set a user agent for Ktor

--- src/main/kotlin/KtLodestone.kt
+++ src/main/kotlin/KtLodestone.kt
@@ -2,5 +2,18 @@ package cloud.drakon.ktlodestone
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.java.Java
+import io.ktor.client.plugins.UserAgent
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 
-internal val ktorClient = HttpClient(Java)
+private val meta = Json.parseToJsonElement(
+    object {}::class.java.classLoader.getResource("lodestone-css-selectors/meta.json") !!
+        .readText()
+)
+
+internal val ktorClient = HttpClient(Java) {
+    install(UserAgent) {
+        agent = meta.jsonObject["userAgentDesktop"] !!.jsonPrimitive.content
+    }
+}
