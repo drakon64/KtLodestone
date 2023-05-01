785657e3c08f77e171ee438798e2ed159a5128cc
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-05-01 17:44:09 +0100

    Optionally use a mobile user agent

--- src/main/kotlin/KtLodestone.kt
+++ src/main/kotlin/KtLodestone.kt
@@ -5,8 +5,9 @@ import cloud.drakon.ktlodestone.exception.LodestoneException
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.java.Java
-import io.ktor.client.plugins.UserAgent
 import io.ktor.client.request.get
+import io.ktor.http.HttpHeaders
+import io.ktor.http.headers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.jsonObject
@@ -18,16 +19,25 @@ private val meta = Json.parseToJsonElement(
         .readText()
 )
 
-internal val ktorClient = HttpClient(Java) {
-    install(UserAgent) {
-        agent = meta.jsonObject["userAgentDesktop"] !!.jsonPrimitive.content
-    }
-}
+private val userAgentDesktop =
+    meta.jsonObject["userAgentDesktop"] !!.jsonPrimitive.content
+private val userAgentMobile =
+    meta.jsonObject["userAgentMobile"] !!.jsonPrimitive.content
+
+internal val ktorClient = HttpClient(Java)
 
 internal suspend fun getLodestoneProfile(id: Int, mobileUserAgent: Boolean = false) =
     coroutineScope {
         val request =
-            ktorClient.get("https://eu.finalfantasyxiv.com/lodestone/character/${id}/")
+            ktorClient.get("https://eu.finalfantasyxiv.com/lodestone/character/${id}/") {
+                headers {
+                    if (! mobileUserAgent) {
+                        append(HttpHeaders.UserAgent, userAgentDesktop)
+                    } else {
+                        append(HttpHeaders.UserAgent, userAgentMobile)
+                    }
+                }
+            }
 
         return@coroutineScope when (request.status.value) {
             200 -> Jsoup.parse(request.body() as String)
