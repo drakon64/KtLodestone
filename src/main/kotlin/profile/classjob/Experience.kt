cf0eaea384defb90177ceedd1d3e48311c5da800
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-05-07 11:53:13 +0100

    Improve documentation for `cloud.drakon.ktlodestone.profile.classjob`

--- src/main/kotlin/profile/classjob/Experience.kt
+++ src/main/kotlin/profile/classjob/Experience.kt
@@ -1,3 +1,8 @@
 package cloud.drakon.ktlodestone.profile.classjob
 
+/**
+ * The experience that a character has.
+ * @property current The experience that the character currently has.
+ * @property next The experience required for the character to reach the next level.
+ */
 data class Experience(val current: Int, val next: Int)
--- src/main/kotlin/profile/classjob/UniqueDutyLevel.kt
+++ src/main/kotlin/profile/classjob/UniqueDutyLevel.kt
@@ -1,6 +1,13 @@
 package cloud.drakon.ktlodestone.profile.classjob
 
+/**
+ * The Elemental Level/Resistance Rank of a character.
+ * @property level The level of a character in the unique duty.
+ * @property experience The experience that a character has in the unique duty.
+ */
 data class UniqueDutyLevel(val level: Byte, val experience: Experience?)
 
-typealias Bozja = UniqueDutyLevel
+/** The Elemental Level of a character. */
 typealias Eureka = UniqueDutyLevel
+/** The Resistance Rank of a character. */
+typealias Bozja = UniqueDutyLevel
