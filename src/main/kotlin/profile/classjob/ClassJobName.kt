e28048cb5383a6acd101f1442920c880731f8866
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-05-07 17:32:07 +0100

    `ProfileClassJob` should contains `Map`s instead of `data class`es

---
+++ src/main/kotlin/profile/classjob/ClassJobName.kt
@@ -0,0 +1,99 @@
+package cloud.drakon.ktlodestone.profile.classjob
+
+/**
+ * An [Enum] who's values consist of job names.
+ */
+enum class ClassJobName {
+    /** Paladin/Gladiator */
+    PALADIN,
+
+    /** Warrior/Marauder */
+    WARRIOR,
+
+    /** Dark Knight */
+    DARKKNIGHT,
+
+    /** Gunbreaker */
+    GUNBREAKER,
+
+    /** White Mage/Conjurer */
+    WHITEMAGE,
+
+    /** Scholar/Arcanist */
+    SCHOLAR,
+
+    /** Astrologian */
+    ASTROLOGIAN,
+
+    /** Sage */
+    SAGE,
+
+    /** Monk/Pugilist */
+    MONK,
+
+    /** Dragoon/Lancer */
+    DRAGOON,
+
+    /** Ninja/Rogue */
+    NINJA,
+
+    /** Samurai */
+    SAMURAI,
+
+    /** Reaper */
+    REAPER,
+
+    /** Bard/Archer */
+    BARD,
+
+    /** Machinist */
+    MACHINIST,
+
+    /** Dancer */
+    DANCER,
+
+    /** Black Mage/Thaumaturge */
+    BLACKMAGE,
+
+    /** Summoner/Arcanist */
+    SUMMONER,
+
+    /** Red Mage */
+    REDMAGE,
+
+    /** Blue Mage */
+    BLUEMAGE,
+
+    /** Carpenter */
+    CARPENTER,
+
+    /** Blacksmith */
+    BLACKSMITH,
+
+    /** Armorer */
+    ARMORER,
+
+    /** Goldsmith */
+    GOLDSMITH,
+
+    /** Leatherworker */
+    LEATHERWORKER,
+
+    /** Weaver */
+    WEAVER,
+
+    /** Alchemist */
+    ALCHEMIST,
+
+    /** Culinarian */
+    CULINARIAN,
+
+    /** Miner */
+    MINER,
+
+    /** Botanist */
+    BOTANIST,
+
+    /** Fisher */
+    FISHER,
+}
---
+++ src/main/kotlin/profile/classjob/UniqueDutyName.kt
@@ -0,0 +1,12 @@
+package cloud.drakon.ktlodestone.profile.classjob
+
+/**
+ * An [Enum] who's values consist of unique duties that appear on a characters Lodestone profile.
+ */
+enum class UniqueDutyName {
+    /** The Forbidden Land: Eureka Anemos/Pagos/Pyros/Hydatos */
+    EUREKA,
+
+    /** Bozjan Southern Front and Zadnor */
+    BOZJA
+}
