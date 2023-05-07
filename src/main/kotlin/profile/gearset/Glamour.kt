322aa95d99ab6799cf06c2755a769521717db5d2
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-05-07 12:01:38 +0100

    Improve documentation for `cloud.drakon.ktlodestone.profile.gearset`

--- src/main/kotlin/profile/gearset/Glamour.kt
+++ src/main/kotlin/profile/gearset/Glamour.kt
@@ -1,3 +1,8 @@
 package cloud.drakon.ktlodestone.profile.gearset
 
+/**
+ * A glamour applied to a gear item.
+ * @property name The name of the glamour item.
+ * @property dbLink The URL to the glamour item in the Eorzea Database.
+ */
 data class Glamour(val name: String, val dbLink: String)
