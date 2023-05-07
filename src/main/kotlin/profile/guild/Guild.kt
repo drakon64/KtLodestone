9368d0d491f05387710b475f386f1c750300bd75
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-05-07 12:29:25 +0100

    Improve documentation for `cloud.drakon.ktlodestone.profile.guild`

--- src/main/kotlin/profile/guild/Guild.kt
+++ src/main/kotlin/profile/guild/Guild.kt
@@ -1,5 +1,11 @@
 package cloud.drakon.ktlodestone.profile.guild
 
+/**
+ * The Free Company/PvP Team that a character is a member of.
+ * @property name The name of the Free Company/PvP Team.
+ * @property id The ID of the Free Company/PvP Team.
+ * @property iconLayers The icon layers of the Free Company/PvP Team.
+ */
 data class Guild(val name: String, val id: String, val iconLayers: IconLayers)
 
 typealias FreeCompany = Guild
