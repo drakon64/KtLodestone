9281496bc2d9a748733073970f131f4ea0187ae5
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-10-09 00:24:49 +0100

    Move `GrandCompany` to the `character.grandcompany` package

--- src/main/kotlin/character/GrandCompany.kt
+++
@@ -1,7 +0,0 @@
-package cloud.drakon.ktlodestone.character
-
-import cloud.drakon.ktlodestone.character.grandcompany.GrandCompanyName
-import cloud.drakon.ktlodestone.character.grandcompany.GrandCompanyRank
-
-/** A [Character]'s Grand Company. */
-data class GrandCompany(val name: GrandCompanyName, val rank: GrandCompanyRank)
