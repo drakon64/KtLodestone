d2391a7752d38ff35134249121e0c2e86cc6f5e7
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-10-09 21:32:23 +0100

    Add classes/jobs to `ClassJob`

--- src/main/kotlin/classjob/ClassJob.kt
+++ src/main/kotlin/classjob/ClassJob.kt
@@ -1,5 +1,203 @@
 package cloud.drakon.ktlodestone.classjob
 
 enum class ClassJob {
+    /** Gladiator */
+    GLADIATOR {
+        override fun toString() = "Gladiator"
+    },
 
+    /** Paladin */
+    PALADIN {
+        override fun toString() = "Paladin"
+    },
+
+    /** Marauder */
+    MARAUDER {
+        override fun toString() = "Marauder"
+    },
+
+    /** Warrior */
+    WARRIOR {
+        override fun toString() = "Warrior"
+    },
+
+    /** Dark Knight */
+    DARK_KNIGHT {
+        override fun toString() = "Dark Knight"
+    },
+
+    /** Gunbreaker */
+    GUNBREAKER {
+        override fun toString() = "Gunbreaker"
+    },
+
+    /** Conjurer */
+    CONJURER {
+        override fun toString() = "Conjurer"
+    },
+
+    /** White Mage */
+    WHITE_MAGE {
+        override fun toString() = "White Mage"
+    },
+
+    /** Scholar */
+    SCHOLAR {
+        override fun toString() = "Scholar"
+    },
+
+    /** Astrologian */
+    ASTROLOGIAN {
+        override fun toString() = "Astrologian"
+    },
+
+    /** Sage */
+    SAGE {
+        override fun toString() = "Sage"
+    },
+
+    /** Pugilist */
+    PUGILIST {
+        override fun toString() = "Pugilist"
+    },
+
+    /** Monk */
+    MONK {
+        override fun toString() = "Monk"
+    },
+
+    /** Lancer */
+    LANCER {
+        override fun toString() = "Lancer"
+    },
+
+    /** Dragoon */
+    DRAGOON {
+        override fun toString() = "Dragoon"
+    },
+
+    /** Rogue */
+    ROGUE {
+        override fun toString() = "Rogue"
+    },
+
+    /** Ninja */
+    NINJA {
+        override fun toString() = "Ninja"
+    },
+
+    /** Samurai */
+    SAMURAI {
+        override fun toString() = "Samurai"
+    },
+
+    /** Reaper */
+    REAPER {
+        override fun toString() = "Reaper"
+    },
+
+    /** Archer */
+    ARCHER {
+        override fun toString() = "Archer"
+    },
+
+    /** Bard */
+    BARD {
+        override fun toString() = "Bard"
+    },
+
+    /** Machinist */
+    MACHINIST {
+        override fun toString() = "Machinist"
+    },
+
+    /** Dancer */
+    DANCER {
+        override fun toString() = "Dancer"
+    },
+
+    /** Thaumaturge */
+    THAUMATURGE {
+        override fun toString() = "Thaumaturge"
+    },
+
+    /** Black Mage */
+    BLACK_MAGE {
+        override fun toString() = "Black Mage"
+    },
+
+    /** Arcanist */
+    ARCANIST {
+        override fun toString() = "Arcanist"
+    },
+
+    /** Summoner */
+    SUMMONER {
+        override fun toString() = "Summoner"
+    },
+
+    /** Red Mage */
+    RED_MAGE {
+        override fun toString() = "Red Mage"
+    },
+
+    /** Blue Mage */
+    BLUE_MAGE {
+        override fun toString() = "Blue Mage"
+    },
+
+    /** Carpenter */
+    CARPENTER {
+        override fun toString() = "Carpenter"
+    },
+
+    /** Blacksmith */
+    BLACKSMITH {
+        override fun toString() = "Blacksmith"
+    },
+
+    /** Armorer */
+    ARMORER {
+        override fun toString() = "Armorer"
+    },
+
+    /** Goldsmith */
+    GOLDSMITH {
+        override fun toString() = "Goldsmith"
+    },
+
+    /** Leatherworker */
+    LEATHERWORKER {
+        override fun toString() = "Leatherworker"
+    },
+
+    /** Weaver */
+    WEAVER {
+        override fun toString() = "Weaver"
+    },
+
+    /** Alchemist */
+    ALCHEMIST {
+        override fun toString() = "Alchemist"
+    },
+
+    /** Culinarian */
+    CULINARIAN {
+        override fun toString() = "Culinarian"
+    },
+
+    /** Miner */
+    MINER {
+        override fun toString() = "Miner"
+    },
+
+    /** Botanist */
+    BOTANIST {
+        override fun toString() = "Botanist"
+    },
+
+    /** Fisher */
+    FISHER {
+        override fun toString() = "Fisher"
+    },
 }
