cdc26de68b4256c2acb3e310fac272e705c0a9d5
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-10-10 21:03:58 +0100

    Add class for storing character search results

---
+++ src/main/kotlin/character/search/CharacterSearchResult.kt
@@ -0,0 +1,17 @@
+package cloud.drakon.ktlodestone.character.search
+
+import cloud.drakon.ktlodestone.character.ActiveClassJob
+import cloud.drakon.ktlodestone.character.profile.grandcompany.GrandCompany
+import cloud.drakon.ktlodestone.world.DataCenter
+import cloud.drakon.ktlodestone.world.World
+
+data class CharacterSearchResult(
+    val avatar: String,
+    val id: Int,
+    val language: List<Language>,
+    val name: String,
+    val grandCompany: GrandCompany?,
+    val world: World,
+    val dataCenter: DataCenter,
+    val activeClassJob: ActiveClassJob
+)
