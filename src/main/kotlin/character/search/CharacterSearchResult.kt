08a7952740d33d9a344e273ea3c982804162bb3f
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-10-11 13:34:42 +0100

    Add KDoc's for `CharacterSearchResult`

--- src/main/kotlin/character/search/CharacterSearchResult.kt
+++ src/main/kotlin/character/search/CharacterSearchResult.kt
@@ -5,12 +5,19 @@ import cloud.drakon.ktlodestone.world.DataCenter
 import cloud.drakon.ktlodestone.world.World
 
 data class CharacterSearchResult(
+    /** The character's avatar. */
     val avatar: String,
+    /** The character's ID. */
     val id: Int,
+    /** The character's list of selected languages. */
     val languages: List<Language>,
+    /** The character's name. */
     val name: String,
+    /** The character's Grand Company. */
     val grandCompany: GrandCompany?,
+    /** The character's world. */
     val world: World,
+    /** The character's data center. */
     val dataCenter: DataCenter,
 //    val activeClassJob: ActiveClassJob
 )
