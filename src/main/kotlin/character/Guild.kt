be217bc5b0d0324c59e29b935516989779a84eba
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-10-11 16:10:20 +0100

    Add KDoc for `Guild`

--- src/main/kotlin/character/Guild.kt
+++ src/main/kotlin/character/Guild.kt
@@ -1,13 +1,20 @@
 package cloud.drakon.ktlodestone.character
 
-import cloud.drakon.ktlodestone.character.profile.CharacterProfile
 import cloud.drakon.ktlodestone.iconlayers.IconLayers
+import cloud.drakon.ktlodestone.searchLodestoneCharacter
 
-/** A character's [FreeCompany] or [PvpTeam]. */
-data class Guild(val name: String, val id: String, val iconLayers: IconLayers?)
+/** A character's Free Company or  PvP Team. */
+data class Guild(
+    /** The name of the Free Company or PvP Team. */
+    val name: String,
+    /** The ID of the Free Company or PvP Team. */
+    val id: String,
+    /** The layers that make up the icon of the Free Company or PvP Team. Always `null` when returned by [searchLodestoneCharacter]. */
+    val iconLayers: IconLayers?,
+)
 
-/** A [CharacterProfile]'s Free Company. */
+/** A character's Free Company. */
 typealias FreeCompany = Guild
 
-/** A [CharacterProfile]'s PvP Team. */
+/** A character's PvP Team. */
 typealias PvpTeam = Guild
