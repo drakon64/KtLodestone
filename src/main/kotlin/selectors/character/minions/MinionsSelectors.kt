0ff3103fec03394863a23626e1ed6a8ce58a7073
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-10-13 22:15:49 +0100

    Remove unused selectors

--- src/main/kotlin/selectors/character/minions/MinionsSelectors.kt
+++ src/main/kotlin/selectors/character/minions/MinionsSelectors.kt
@@ -4,6 +4,4 @@ internal object MinionsSelectors {
     const val ROOT = ".minion__list__item"
 
     const val NAME = ".minion__name"
-
-    const val TOTAL = ".minion__sort__total > span:nth-child(1)"
 }
