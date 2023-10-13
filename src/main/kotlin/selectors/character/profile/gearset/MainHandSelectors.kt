87f07621a1abf5fd5f465556ed8774f8105c9f7b
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-10-13 11:04:29 +0100

    Return glamours, dyes, materia, and item creator

--- src/main/kotlin/selectors/character/profile/gearset/MainHandSelectors.kt
+++ src/main/kotlin/selectors/character/profile/gearset/MainHandSelectors.kt
@@ -6,4 +6,15 @@ object MainHandSelectors {
 
     const val DB_LINK = ".icon-c--0 > .db-tooltip > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > a:nth-child(1)"
     const val DB_LINK_ATTR = "href"
+
+    const val GLAMOUR = ".icon-c--0 > .db-tooltip > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > p:nth-child(2)"
+    const val GLAMOUR_DB_LINK = ".icon-c--0 > .db-tooltip > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > p:nth-child(2) > a:nth-child(1)"
+    const val GLAMOUR_DB_LINK_ATTR = "href"
+
+    const val DYE = ".icon-c--0 > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > div:nth-child(5) > div:nth-child(3) > div:nth-child(1) > a:nth-child(1)"
+
+    const val MATERIA_1 = ".icon-c--0 > .db-tooltip > div:nth-child(1) > div:nth-child(1) > div:nth-child(5) > ul:nth-of-type(2) > li:nth-child(1) > div:nth-child(2)"
+    val MATERIA_REGEX = """.*(?=\n<br>)""".toRegex()
+
+    const val CREATOR_NAME = ".icon-c--0 > .db-tooltip > div:nth-child(1) > div:nth-child(1) > div:nth-child(7) > div:nth-child(1) > a:nth-child(1)"
 }
