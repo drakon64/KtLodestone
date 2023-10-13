fbf55d08d10d45fa13a84387f6fac3a5ae7d66d1
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-10-13 19:52:10 +0100

    Comment that materia selectors apply from the root level

--- src/main/kotlin/selectors/character/profile/gearset/BraceletsSelectors.kt
+++ src/main/kotlin/selectors/character/profile/gearset/BraceletsSelectors.kt
@@ -16,6 +16,7 @@ object BraceletsSelectors : GearSetSelectors {
 
     override val DYE = ".icon-c--10 > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > div:nth-child(5) > div:nth-child(3) > div:nth-child(1) > a:nth-child(1)"
 
+    // Materia selectors have to be from the root level
     override val MATERIA_1 = ".icon-c--10 > .db-tooltip > div:nth-child(1) > div:nth-child(1) > div:nth-child(5) > ul:nth-of-type(2) > li:nth-child(1) > div:nth-child(2)"
     override val MATERIA_2 = ".icon-c--10 > .db-tooltip > div:nth-child(1) > div:nth-child(1) > div:nth-child(5) > ul:nth-of-type(2) > li:nth-child(2) > div:nth-child(2)"
     override val MATERIA_3 = ".icon-c--10 > .db-tooltip > div:nth-child(1) > div:nth-child(1) > div:nth-child(5) > ul:nth-of-type(2) > li:nth-child(3) > div:nth-child(2)"
