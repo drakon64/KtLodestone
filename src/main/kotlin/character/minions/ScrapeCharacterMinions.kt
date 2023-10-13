c1a277c0b97ab83601ea4da5cecec9ade9e315e1
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-10-13 22:24:27 +0100

    Further simplify functions

--- src/main/kotlin/character/minions/ScrapeCharacterMinions.kt
+++ src/main/kotlin/character/minions/ScrapeCharacterMinions.kt
@@ -1,15 +1,13 @@
 package cloud.drakon.ktlodestone.character.minions
 
 import cloud.drakon.ktlodestone.selectors.character.minions.MinionsSelectors
-import kotlinx.coroutines.coroutineScope
 import org.jsoup.Jsoup
 
-internal suspend fun scrapeCharacterMinions(response: String) = Jsoup.parse(response).let {
-    it.select(MinionsSelectors.ROOT).let {
+internal fun scrapeCharacterMinions(response: String) = Jsoup.parse(response)
+    .select(MinionsSelectors.ROOT).let {
         buildList {
             it.forEach {
                 add(it.select(MinionsSelectors.NAME).text())
             }
         }
     }
-}
