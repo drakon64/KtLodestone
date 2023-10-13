5313f7fe64b83388d9ef1890a1cd244331d20c69
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-10-13 21:48:06 +0100

    Return a characters unlocked minions

---
+++ src/main/kotlin/selectors/character/minions/MinionsSelectors.kt
@@ -0,0 +1,9 @@
+package cloud.drakon.ktlodestone.selectors.character.minions
+
+internal object MinionsSelectors {
+    const val ROOT = ".minion__list__item"
+
+    const val NAME = ".minion__name"
+
+    const val TOTAL = ".minion__sort__total > span:nth-child(1)"
+}
