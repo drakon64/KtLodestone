b18f3304c77773eaeeccc50ac72d50fd67aabc3c
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-10-13 21:03:07 +0100

    Remove `freeCompany` and `pvpTeam` type aliases

--- src/main/kotlin/character/Guild.kt
+++ src/main/kotlin/character/Guild.kt
@@ -3,7 +3,7 @@ package cloud.drakon.ktlodestone.character
 import cloud.drakon.ktlodestone.iconlayers.IconLayers
 import cloud.drakon.ktlodestone.searchLodestoneCharacter
 
-/** A character's Free Company or  PvP Team. */
+/** A character's Free Company or PvP Team. */
 data class Guild(
     /** The name of the Free Company or PvP Team. */
     val name: String,
@@ -12,9 +12,3 @@ data class Guild(
     /** The layers that make up the icon of the Free Company or PvP Team. Always `null` when returned by [searchLodestoneCharacter]. */
     val iconLayers: IconLayers?,
 )
-
-/** A character's Free Company. */
-typealias FreeCompany = Guild
-
-/** A character's PvP Team. */
-typealias PvpTeam = Guild
