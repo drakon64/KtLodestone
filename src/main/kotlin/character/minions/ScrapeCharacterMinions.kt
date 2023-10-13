b616ff52aa7bd541affb2dc7f487705445a7613c
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-10-13 22:18:36 +0100

    Remove unnecessary coroutine scopes

--- src/main/kotlin/character/minions/ScrapeCharacterMinions.kt
+++ src/main/kotlin/character/minions/ScrapeCharacterMinions.kt
@@ -4,13 +4,11 @@ import cloud.drakon.ktlodestone.selectors.character.minions.MinionsSelectors
 import kotlinx.coroutines.coroutineScope
 import org.jsoup.Jsoup
 
-internal suspend fun scrapeCharacterMinions(response: String) = coroutineScope {
-    Jsoup.parse(response).let {
-        it.select(MinionsSelectors.ROOT).let {
-            buildList {
-                it.forEach {
-                    add(it.select(MinionsSelectors.NAME).text())
-                }
+internal suspend fun scrapeCharacterMinions(response: String) = Jsoup.parse(response).let {
+    it.select(MinionsSelectors.ROOT).let {
+        buildList {
+            it.forEach {
+                add(it.select(MinionsSelectors.NAME).text())
             }
         }
     }
