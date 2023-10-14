4dc3290e76485b4c6fef67658588350b92fa2955
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-10-14 19:17:02 +0100

    Move `scrapeSearchResults` to a companion object

--- src/main/kotlin/character/search/CharacterSearch.kt
+++ src/main/kotlin/character/search/CharacterSearch.kt
@@ -23,6 +23,157 @@ import org.jsoup.select.Elements
 
 // To ensure that `nextPage` isn't accessed statically, these functions get their own class
 internal class CharacterSearch {
+    private companion object {
+        // This function can be accessed statically
+        private suspend fun scrapeSearchResults(elements: Elements) = coroutineScope {
+            buildList {
+                elements.select(CharacterSearchSelectors.ENTRIES_ROOT).forEach {
+                    val avatar = async {
+                        it.select(CharacterSearchSelectors.ENTRY_AVATAR)
+                            .attr(CharacterSearchSelectors.ENTRY_AVATAR_ATTR)
+                    }
+
+                    val id = async {
+                        it.select(CharacterSearchSelectors.ENTRY_ID)
+                            .attr(CharacterSearchSelectors.ENTRY_ID_ATTR)
+                            .split("/")[3]
+                            .toInt()
+                    }
+
+                    val language = async {
+                        buildList {
+                            it.select(CharacterSearchSelectors.ENTRY_LANGUAGE).text().let {
+                                if (it.contains("JA")) {
+                                    add(Language.JAPANESE)
+                                }
+
+                                if (it.contains("EN")) {
+                                    add(Language.ENGLISH)
+                                }
+
+                                if (it.contains("DE")) {
+                                    add(Language.GERMAN)
+                                }
+
+                                if (it.contains("FR")) {
+                                    add(Language.FRENCH)
+                                }
+                            }
+                        }
+                    }
+
+                    val name = async {
+                        it.select(CharacterSearchSelectors.ENTRY_NAME).text()
+                    }
+
+                    val grandCompany = async {
+                        it.select(CharacterSearchSelectors.ENTRY_GRAND_COMPANY_RANK)
+                            .first()
+                            ?.attr(CharacterSearchSelectors.ENTRY_GRAND_COMPANY_RANK_ATTR)
+                            ?.let {
+                                val grandCompanyName = async {
+                                    GrandCompanyName.valueOf(
+                                        it.split("/")[0]
+                                            .trim()
+                                            .replace(" ", "_")
+                                            .uppercase()
+                                    )
+                                }
+
+                                val grandCompanyRank = async {
+                                    GrandCompanyRank.valueOf(
+                                        it.split("/")[1]
+                                            .trim()
+                                            .replace(" ", "_")
+                                            .uppercase()
+                                    )
+                                }
+
+                                GrandCompany(
+                                    grandCompanyName.await(),
+                                    grandCompanyRank.await()
+                                )
+                            }
+                    }
+
+                    val freeCompany = async {
+                        it.select(CharacterSearchSelectors.ENTRY_FREE_COMPANY_ID)
+                            .first()
+                            ?.let {
+                                val freeCompanyName = async {
+                                    it.select(CharacterSearchSelectors.ENTRY_FREE_COMPANY_NAME)
+                                        .text()
+                                }
+
+                                val freeCompanyId = async {
+                                    it.attr(CharacterSearchSelectors.ENTRY_FREE_COMPANY_ID_ATTR)
+                                        .split("/")[3]
+                                }
+
+                                Guild(freeCompanyName.await(), freeCompanyId.await(), null)
+                            }
+                    }
+
+                    val world = async {
+                        World.valueOf(
+                            it.select(CharacterSearchSelectors.ENTRY_WORLD)
+                                .text()
+                                .split("[")[0]
+                                .trim()
+                        )
+                    }
+
+                    val dataCenter = async {
+                        DataCenter.valueOf(
+                            it.select(CharacterSearchSelectors.ENTRY_WORLD)
+                                .text()
+                                .split("[")[1]
+                                .replace("]", "")
+                        )
+                    }
+
+                    val region = async {
+                        CharacterProfileMaps.REGION_MAP.getValue(dataCenter.await())
+                    }
+
+                    val activeClassJob = async {
+                        it.select(CharacterSearchSelectors.ENTRY_CHARA_INFO).let {
+                            val classJob = async {
+                                CharacterProfileMaps.CLASS_JOB_MAP.getValue(
+                                    it.select(CharacterSearchSelectors.ENTRY_ACTIVE_CLASSJOB)
+                                        .attr(CharacterSearchSelectors.ENTRY_ACTIVE_CLASSJOB_ATTR)
+                                )
+                            }
+
+                            val level = async {
+                                it.select(CharacterSearchSelectors.ENTRY_ACTIVE_CLASSJOB_LEVEL)
+                                    .text()
+                                    .toByte()
+                            }
+
+                            ActiveClassJob(classJob.await(), level.await())
+                        }
+                    }
+
+                    add(
+                        CharacterSearchResult(
+                            avatar.await(),
+                            id.await(),
+                            language.await(),
+                            name.await(),
+                            grandCompany.await(),
+                            freeCompany.await(),
+                            world.await(),
+                            dataCenter.await(),
+                            region.await(),
+                            activeClassJob.await(),
+                        )
+                    )
+                }
+            }
+        }
+    }
+
     private var nextPage = true
 
     internal suspend fun scrapeCharacterSearch(
@@ -123,155 +274,6 @@ internal class CharacterSearch {
             }
 
             else -> throw LodestoneException()
-        }
-    }
-}
-
-// This function can be accessed statically
-private suspend fun scrapeSearchResults(elements: Elements) = coroutineScope {
-    buildList {
-        elements.select(CharacterSearchSelectors.ENTRIES_ROOT).forEach {
-            val avatar = async {
-                it.select(CharacterSearchSelectors.ENTRY_AVATAR)
-                    .attr(CharacterSearchSelectors.ENTRY_AVATAR_ATTR)
-            }
-
-            val id = async {
-                it.select(CharacterSearchSelectors.ENTRY_ID)
-                    .attr(CharacterSearchSelectors.ENTRY_ID_ATTR)
-                    .split("/")[3]
-                    .toInt()
-            }
-
-            val language = async {
-                buildList {
-                    it.select(CharacterSearchSelectors.ENTRY_LANGUAGE).text().let {
-                        if (it.contains("JA")) {
-                            add(Language.JAPANESE)
-                        }
-
-                        if (it.contains("EN")) {
-                            add(Language.ENGLISH)
-                        }
-
-                        if (it.contains("DE")) {
-                            add(Language.GERMAN)
-                        }
-
-                        if (it.contains("FR")) {
-                            add(Language.FRENCH)
-                        }
-                    }
-                }
-            }
-
-            val name = async {
-                it.select(CharacterSearchSelectors.ENTRY_NAME).text()
-            }
-
-            val grandCompany = async {
-                it.select(CharacterSearchSelectors.ENTRY_GRAND_COMPANY_RANK)
-                    .first()
-                    ?.attr(CharacterSearchSelectors.ENTRY_GRAND_COMPANY_RANK_ATTR)
-                    ?.let {
-                        val grandCompanyName = async {
-                            GrandCompanyName.valueOf(
-                                it.split("/")[0]
-                                    .trim()
-                                    .replace(" ", "_")
-                                    .uppercase()
-                            )
-                        }
-
-                        val grandCompanyRank = async {
-                            GrandCompanyRank.valueOf(
-                                it.split("/")[1]
-                                    .trim()
-                                    .replace(" ", "_")
-                                    .uppercase()
-                            )
-                        }
-
-                        GrandCompany(
-                            grandCompanyName.await(),
-                            grandCompanyRank.await()
-                        )
-                    }
-            }
-
-            val freeCompany = async {
-                it.select(CharacterSearchSelectors.ENTRY_FREE_COMPANY_ID)
-                    .first()
-                    ?.let {
-                        val freeCompanyName = async {
-                            it.select(CharacterSearchSelectors.ENTRY_FREE_COMPANY_NAME)
-                                .text()
-                        }
-
-                        val freeCompanyId = async {
-                            it.attr(CharacterSearchSelectors.ENTRY_FREE_COMPANY_ID_ATTR)
-                                .split("/")[3]
-                        }
-
-                        Guild(freeCompanyName.await(), freeCompanyId.await(), null)
-                    }
-            }
-
-            val world = async {
-                World.valueOf(
-                    it.select(CharacterSearchSelectors.ENTRY_WORLD)
-                        .text()
-                        .split("[")[0]
-                        .trim()
-                )
-            }
-
-            val dataCenter = async {
-                DataCenter.valueOf(
-                    it.select(CharacterSearchSelectors.ENTRY_WORLD)
-                        .text()
-                        .split("[")[1]
-                        .replace("]", "")
-                )
-            }
-
-            val region = async {
-                CharacterProfileMaps.REGION_MAP.getValue(dataCenter.await())
-            }
-
-            val activeClassJob = async {
-                it.select(CharacterSearchSelectors.ENTRY_CHARA_INFO).let {
-                    val classJob = async {
-                        CharacterProfileMaps.CLASS_JOB_MAP.getValue(
-                            it.select(CharacterSearchSelectors.ENTRY_ACTIVE_CLASSJOB)
-                                .attr(CharacterSearchSelectors.ENTRY_ACTIVE_CLASSJOB_ATTR)
-                        )
-                    }
-
-                    val level = async {
-                        it.select(CharacterSearchSelectors.ENTRY_ACTIVE_CLASSJOB_LEVEL)
-                            .text()
-                            .toByte()
-                    }
-
-                    ActiveClassJob(classJob.await(), level.await())
-                }
-            }
-
-            add(
-                CharacterSearchResult(
-                    avatar.await(),
-                    id.await(),
-                    language.await(),
-                    name.await(),
-                    grandCompany.await(),
-                    freeCompany.await(),
-                    world.await(),
-                    dataCenter.await(),
-                    region.await(),
-                    activeClassJob.await(),
-                )
-            )
         }
     }
 }
