b47e1051e64afb8b441bdc4caff08810cc32cdac
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-10-15 13:37:08 +0100

    Reduce duplicate map entries

--- src/main/kotlin/character/classjob/ClassJobLevel.kt
+++ src/main/kotlin/character/classjob/ClassJobLevel.kt
@@ -1,9 +1,6 @@
 package cloud.drakon.ktlodestone.character.classjob
 
-import cloud.drakon.ktlodestone.character.ClassJob
-
 data class ClassJobLevel(
-    val unlockState: ClassJob,
     val level: Byte,
-    val experience: Experience?
+    val experience: Experience?,
 )
