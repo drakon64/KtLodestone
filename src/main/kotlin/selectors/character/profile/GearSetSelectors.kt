aa8ee35d1b223c6f5742cb1fa829d38dd748c1b8
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-10-16 23:15:10 +0100

    Remove comment

--- src/main/kotlin/selectors/character/profile/GearSetSelectors.kt
+++ src/main/kotlin/selectors/character/profile/GearSetSelectors.kt
@@ -21,7 +21,6 @@ internal class GearSetSelectors(val icon: Byte) {
 
     val DYE = ".icon-c--$icon > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > div:nth-child(5) > div:nth-child(3) > div:nth-child(1) > a:nth-child(1)"
 
-    // Materia selectors have to be from the root level
     val MATERIA_1 = ".icon-c--$icon > .db-tooltip > div:nth-child(1) > div:nth-child(1) > div:nth-child(5) > ul:nth-of-type(2) > li:nth-child(1) > div:nth-child(2)"
     val MATERIA_2 = ".icon-c--$icon > .db-tooltip > div:nth-child(1) > div:nth-child(1) > div:nth-child(5) > ul:nth-of-type(2) > li:nth-child(2) > div:nth-child(2)"
     val MATERIA_3 = ".icon-c--$icon > .db-tooltip > div:nth-child(1) > div:nth-child(1) > div:nth-child(5) > ul:nth-of-type(2) > li:nth-child(3) > div:nth-child(2)"
