a782222b09145e5022a8a9ef34c0f79b309913b6
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-10-17 19:36:37 +0100

    Formatting

--- src/main/kotlin/character/profile/ScrapeCharacterProfile.kt
+++ src/main/kotlin/character/profile/ScrapeCharacterProfile.kt
@@ -159,8 +159,8 @@ internal suspend fun scrapeCharacterProfile(response: String) = coroutineScope {
                 getGearSetItem(
                     it, GearSetSelectors(
                         byteArrayOf(
-                            0, 1, 2, 3, 4,
-                            6, 7, 8, 9, 10, 11, 12, 13
+                            0, 1, 2, 3, 4, 6,
+                            7, 8, 9, 10, 11, 12, 13
                         )[slot]
                     )
                 )
