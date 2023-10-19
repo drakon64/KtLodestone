4c7c58ea2f5907aba7ac8aeb67e498d28ec9f727
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-10-19 20:58:28 +0100

    Remove broken test

--- src/test/kotlin/linkshell/GetLodestoneLinkshellTest.kt
+++ src/test/kotlin/linkshell/GetLodestoneLinkshellTest.kt
@@ -7,13 +7,6 @@ import org.junit.jupiter.api.assertDoesNotThrow
 
 class GetLodestoneLinkshellTest {
     @Test
-    fun getLodestoneLinkshellTest() = assertDoesNotThrow {
-        runBlocking {
-            println(getLodestoneLinkshell("307846eb43875f693b5fa713f2cdf57e79b8e74e"))
-        }
-    }
-
-    @Test
     fun getLodestoneCrossWorldLinkshellTest() = assertDoesNotThrow {
         runBlocking {
             println(
