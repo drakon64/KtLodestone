0cdde45df8d4478d9a5d9b4f98c743ba1e668152
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-10-19 22:33:30 +0100

    Add KDoc for `LinkshellMember`

--- src/main/kotlin/linkshell/LinkshellMember.kt
+++ src/main/kotlin/linkshell/LinkshellMember.kt
@@ -6,12 +6,20 @@ import cloud.drakon.ktlodestone.world.Region
 import cloud.drakon.ktlodestone.world.World
 
 data class LinkshellMember(
+    /** The Linkshell members name. */
     val name: String,
+    /** The character ID of the Linkshell member. */
     val id: Int,
+    /** The Linkshell members avatar. */
     val avatar: String,
+    /** The Linkshell members Grand Company. */
     val grandCompany: GrandCompany?,
+    /** The Linkshell members rank within the Linkshell. */
     val linkshellRank: String?,
+    /** The Linkshell members world. */
     val world: World,
+    /** The Linkshell members data center. */
     val dataCenter: DataCenter,
+    /** The Linkshell members region. */
     val region: Region,
 )
