9927f7d6181ac69140fbc58967ad3bc4c924b1f0
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-11-29 16:06:04 +0000

    Update Lodestone URL's https://eu.finalfantasyxiv.com/lodestone/news/detail/8851fd4429f783487e5fd66ca14b23e0be145d31

--- src/main/kotlin/selectors/freecompany/search/FreeCompanySearchMaps.kt
+++ src/main/kotlin/selectors/freecompany/search/FreeCompanySearchMaps.kt
@@ -5,22 +5,22 @@ import cloud.drakon.ktlodestone.freecompany.search.Seeking
 
 internal object FreeCompanySearchMaps {
     val FOCUS_MAP = mapOf(
-        "https://img.finalfantasyxiv.com/lds/h/9/2RIcg3Swu7asLE9w5hF11Gm1Sg.png" to Focus.ROLE_PLAYING,
-        "https://img.finalfantasyxiv.com/lds/h/n/5Y0D3iH7ngHlRpv9-KJKalt3_o.png" to Focus.LEVELING,
-        "https://img.finalfantasyxiv.com/lds/h/t/Se_50UHNcTNjytzG8olOSGq3MI.png" to Focus.CASUAL,
-        "https://img.finalfantasyxiv.com/lds/h/i/1ZVA5nBRhczPW2kJWBJk5Jkz6o.png" to Focus.HARDCORE,
-        "https://img.finalfantasyxiv.com/lds/h/7/jSYmuDYlpVzQ98RA3VhBQWY5PA.png" to Focus.DUNGEONS,
-        "https://img.finalfantasyxiv.com/lds/h/C/wiboHfsKd8h_tmbJkNY38w2T8A.png" to Focus.GUILDHESTS,
-        "https://img.finalfantasyxiv.com/lds/h/P/ydn7ovgdfDc3MocTJV0vP48l4E.png" to Focus.TRIALS,
-        "https://img.finalfantasyxiv.com/lds/h/R/R7mn0-cTBIhT8CcxbtDB-YuPUI.png" to Focus.RAIDS,
-        "https://img.finalfantasyxiv.com/lds/h/D/hXSGh69WuUbiC5NmlZhDyaO2g8.png" to Focus.PVP,
+        "https://lds-img.finalfantasyxiv.com/h/9/2RIcg3Swu7asLE9w5hF11Gm1Sg.png" to Focus.ROLE_PLAYING,
+        "https://lds-img.finalfantasyxiv.com/h/n/5Y0D3iH7ngHlRpv9-KJKalt3_o.png" to Focus.LEVELING,
+        "https://lds-img.finalfantasyxiv.com/h/t/Se_50UHNcTNjytzG8olOSGq3MI.png" to Focus.CASUAL,
+        "https://lds-img.finalfantasyxiv.com/h/i/1ZVA5nBRhczPW2kJWBJk5Jkz6o.png" to Focus.HARDCORE,
+        "https://lds-img.finalfantasyxiv.com/h/7/jSYmuDYlpVzQ98RA3VhBQWY5PA.png" to Focus.DUNGEONS,
+        "https://lds-img.finalfantasyxiv.com/h/C/wiboHfsKd8h_tmbJkNY38w2T8A.png" to Focus.GUILDHESTS,
+        "https://lds-img.finalfantasyxiv.com/h/P/ydn7ovgdfDc3MocTJV0vP48l4E.png" to Focus.TRIALS,
+        "https://lds-img.finalfantasyxiv.com/h/R/R7mn0-cTBIhT8CcxbtDB-YuPUI.png" to Focus.RAIDS,
+        "https://lds-img.finalfantasyxiv.com/h/D/hXSGh69WuUbiC5NmlZhDyaO2g8.png" to Focus.PVP,
     )
 
     val SEEKING_MAP = mapOf(
-        "https://img.finalfantasyxiv.com/lds/h/x/VUfyJ-2iArKs72p4putqJewSgU.png" to Seeking.TANK,
-        "https://img.finalfantasyxiv.com/lds/h/A/a8JOc9-8I-ATxTnT3BAkI0MTsA.png" to Seeking.HEALER,
-        "https://img.finalfantasyxiv.com/lds/h/d/qXomRVXuqEzMLXk2ncLeoFTFYs.png" to Seeking.DPS,
-        "https://img.finalfantasyxiv.com/lds/h/w/h-PrIjVyD8Uy9ndk49alidJYSM.png" to Seeking.CRAFTER,
-        "https://img.finalfantasyxiv.com/lds/h/1/7Fd00rEgUDpmWFVulcouYytj54.png" to Seeking.GATHERER,
+        "https://lds-img.finalfantasyxiv.com/h/x/VUfyJ-2iArKs72p4putqJewSgU.png" to Seeking.TANK,
+        "https://lds-img.finalfantasyxiv.com/h/A/a8JOc9-8I-ATxTnT3BAkI0MTsA.png" to Seeking.HEALER,
+        "https://lds-img.finalfantasyxiv.com/h/d/qXomRVXuqEzMLXk2ncLeoFTFYs.png" to Seeking.DPS,
+        "https://lds-img.finalfantasyxiv.com/h/w/h-PrIjVyD8Uy9ndk49alidJYSM.png" to Seeking.CRAFTER,
+        "https://lds-img.finalfantasyxiv.com/h/1/7Fd00rEgUDpmWFVulcouYytj54.png" to Seeking.GATHERER,
     )
 }
